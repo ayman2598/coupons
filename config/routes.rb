f50220618109c5b1ce143c3a6d80cbc5681b801d
ayman2598 <42175687+ayman2598@users.noreply.github.com>
2020-10-27 07:40:32 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Coupons::Engine.routes.draw do
   patch '/coupons', to: 'coupons#batch'
   get '/coupons/apply', to: 'coupons#apply', as: 'apply_coupon', format: 'json'
-
+mount Coupons::Engine => '/', as: 'coupons_engine'
   resources :coupons do
     get :remove, on: :member
     get :duplicate, on: :member
